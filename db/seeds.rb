620b830290339eb12fc2dd47d0528a6817f76862
William Barela <william.barela.dev@gmail.com>
2019-04-28 00:09:56 -0500

    add objectives statement

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,5 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+#
+Staff.create(first_name: 'William', last_name: 'Barela', objectives_statement: 'My objective is to improve the lives of others through the development of optimized applications and ecosystems which are sustainable, scableable and replicative.')
