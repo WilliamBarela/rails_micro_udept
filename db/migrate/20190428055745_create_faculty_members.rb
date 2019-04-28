ba739e806a852538038fcf91db5a6edde6e1829e
William Barela <william.barela.dev@gmail.com>
2019-04-28 02:03:06 -0500

    add comments to faculty_members, adjusted data types

--- db/migrate/20190428055745_create_faculty_members.rb
+++ db/migrate/20190428055745_create_faculty_members.rb
@@ -1,13 +1,13 @@
 class CreateFacultyMembers < ActiveRecord::Migration[5.2]
   def change
-    create_table :faculty_members do |t|
-      t.string :current_title
-      t.string :auxiliary_roles
-      t.string :objectives_statement
-      t.string :research_interests_summary
-      t.date :role_termination_date
-      t.string :termination_reason
-      t.references :person, foreign_key: true
+    create_table :faculty_members, comment: "FacultyMembers table for all faculty member roles" do |t|
+      t.string :current_title, comment: "Professor, Assistant Professor, Instructor, ..."
+      t.text :auxiliary_roles, comment: "Chairship, Directorship, ..."
+      t.text :objectives_statement, comment: "160 or less character long objectives/mission statement"
+      t.text :research_interests_summary, comment: "paragraph(s) stating summary of research interests"
+      t.date :role_termination_date, comment: "date in which faculty role finished"
+      t.string :termination_reason, comment: "default: null; contract ended, fired, quit, ..."
+      t.references :person, foreign_key: true, comment: "one person to possibly many faculty roles"
 
       t.timestamps
     end
