3a7ea10f657c770cab2d7232754d08558d99d361
William Barela <william.barela.dev@gmail.com>
2019-04-28 21:19:09 -0500

    add committee model and comments

---
+++ db/migrate/20190429021444_create_committees.rb
@@ -0,0 +1,12 @@
+class CreateCommittees < ActiveRecord::Migration[5.2]
+  def change
+    create_table :committees do |t|
+      t.string :committee_title, comment: "title of departmental committee"
+      t.text :committee_description, comment: "description of the purpose of committee"
+      t.date :committee_started, comment: "date that the committee was initiated"
+      t.date :committee_depricated, comment: "date that the comittee was depricated"
+
+      t.timestamps
+    end
+  end
+end
