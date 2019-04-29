10d684c6b4ec8b94532480cc9ea4a98f87831909
William Barela <william.barela.dev@gmail.com>
2019-04-29 00:18:03 -0500

    add has_many association

--- app/models/person.rb
+++ app/models/person.rb
@@ -2,5 +2,7 @@ class Person < ApplicationRecord
   has_many :faculty_members
   has_many :staff_members
   has_many :graduate_students
+
+  has_many :committee_members                       # semantically this would be better with committee_memberships...
   has_many :committees, through: :committee_members # change to committee_memberships to improve semantics?
 end
