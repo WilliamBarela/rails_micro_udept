73b0da2c402844f44624b9d8517ca6769813c5bc
William Barela <william.barela.dev@gmail.com>
2019-04-29 00:13:32 -0500

    add associations to person model

--- app/models/person.rb
+++ app/models/person.rb
@@ -1,2 +1,6 @@
 class Person < ApplicationRecord
+  has_many :faculty_members
+  has_many :staff_members
+  has_many :graduate_students
+  has_many :committees, through: :committee_members # change to committee_memberships to improve semantics?
 end
