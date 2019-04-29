b0c8924a3d59bd878c3ea33fa0f63dd6ed5a9783
William Barela <william.barela.dev@gmail.com>
2019-04-29 10:54:17 -0500

    add graduate_memberships associations in faculty_members and graduate_students

--- app/models/faculty_member.rb
+++ app/models/faculty_member.rb
@@ -1,3 +1,6 @@
 class FacultyMember < ApplicationRecord
   belongs_to :person
+
+  has_many :graduate_advisorships
+  has_many :graduate_students, through: :graduate_advisorships
 end
--- app/models/graduate_student.rb
+++ app/models/graduate_student.rb
@@ -1,3 +1,6 @@
 class GraduateStudent < ApplicationRecord
   belongs_to :person
+
+  has_many :graduate_advisorships
+  has_many :faculty_members, through: :graduate_advisorships
 end
