106983585e982bea1aa45fba75225306ec7724c1
William Barela <william.barela.dev@gmail.com>
2019-04-30 16:14:38 -0500

    correct person migration boolean to date

--- specifications/generators.sh
+++ specifications/generators.sh
@@ -1,5 +1,5 @@
 # generate Person model
-rails g model Person honorific:string last_name:string middle_name:string first_name:string suffix:string gender:string ttu_email:string ttu_phone:string ttu_termination:boolean termination_reason:string
+rails g model Person honorific:string last_name:string middle_name:string first_name:string suffix:string gender:string ttu_email:string ttu_phone:string ttu_termination:date termination_reason:string
 
 # generate FacultyMember model
 rails g model FacultyMember current_title:string auxiliary_roles:text objectives_statement:text research_interests_summary:text role_termination_date:date termination_reason:string person:references{index}
