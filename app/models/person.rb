37d5831c50a46fb78a88feb1deab3628255edba4
William Barela <william.barela.dev@gmail.com>
2019-04-30 15:39:09 -0500

    add person validations for name items

--- app/models/person.rb
+++ app/models/person.rb
@@ -5,4 +5,8 @@ class Person < ApplicationRecord
 
   has_many :committee_memberships
   has_many :committees, through: :committee_memberships
+
+  validates :honorific, length: { minimum: 2 }
+  validates :last_name, :middle_name, :first_name, length: { 2..50 }
+  validates :suffix, length: { 1..4 }
 end
