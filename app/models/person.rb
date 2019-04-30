68a50a4c3fcb7d941f5c3501e258c3fadc5ff226
William Barela <william.barela.dev@gmail.com>
2019-04-30 16:41:09 -0500

    add string validations to person

--- app/models/person.rb
+++ app/models/person.rb
@@ -6,7 +6,15 @@ class Person < ApplicationRecord
   has_many :committee_memberships
   has_many :committees, through: :committee_memberships
 
-  validates :honorific, length: { minimum: 2 }
-  validates :last_name, :middle_name, :first_name, length: { 2..50 }
+  # string validations
+  validates :honorific, :last_name, :middle_name, :first_name, :suffix, :gender, :termination_reason,
+    format: { with: /\A[a-zA-Z-]+\z/, message: "only letters and hyphens allowed" }
+  validates :honorific,
+    length: { minimum: 2 }
+  validates :last_name, :middle_name, :first_name,
+    length: { 2..50 }
   validates :suffix, length: { 1..4 }
 end
+
+
+
