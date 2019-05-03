28f6a035c03313d4b5b7f687ff5602de1012cda1
William Barela <william.barela.dev@gmail.com>
2019-05-03 17:20:58 -0500

    add string_with_space validation to committee_description

--- app/models/committee.rb
+++ app/models/committee.rb
@@ -2,7 +2,8 @@ class Committee < ApplicationRecord
   has_many :committee_memberships
   has_many :people, through: :committee_memberships
 
-  validates :committee_title, presence: true, string_with_space: true
+  validates :committee_title, :committee_description,
+    presence: true, string_with_space: true
 
   # fields to validate
   # :committee_title
