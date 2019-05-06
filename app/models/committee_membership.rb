550ad0295448baf8c2978a5537277a83f18f0b1a
William Barela <william.barela.dev@gmail.com>
2019-05-06 13:48:15 -0500

    add join_preceeds_termination validation

--- app/models/committee_membership.rb
+++ app/models/committee_membership.rb
@@ -2,10 +2,14 @@ class CommitteeMembership < ApplicationRecord
   belongs_to :person
   belongs_to :committee
 
-  # validations to complete
-  # :role
-  # :join_date
-  # :termination_date
-  # :person
-  # :committee
+  validates :role,
+    presence: true, string_with_space: true
+
+  validate :join_proceeds_termination
+
+  def join_proceeds_termination
+    unless self.termination_date.nil? or self.termination_date > self.join_date
+      errors.add(:termination_date, "termination date cannot be prior to join date")
+    end
+  end
 end
