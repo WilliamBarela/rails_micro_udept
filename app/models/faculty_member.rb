a6e7dec335d565ff4822ecc2c8eb64d100d65bff
William Barela <william.barela.dev@gmail.com>
2019-05-09 18:28:08 -0500

    add validations to faculty_member.rb

--- app/models/faculty_member.rb
+++ app/models/faculty_member.rb
@@ -4,12 +4,28 @@ class FacultyMember < ApplicationRecord
   has_many :graduate_advisorships
   has_many :graduate_students, through: :graduate_advisorships
 
-  # items to validate
-  # :current_title
-  # :auxiliary_roles
-  # :objectives_statement
-  # :research_interests_summary
-  # :role_termination_date
-  # :termination_reason
-  # :person
+  validates :current_title, :auxiliary_roles, :termination_reason,
+    presence: true,
+    string_with_space: true,
+    length: { maximum: 255 },
+    allow_nil: false
+
+  validates :objectives_statement
+    presence: true,
+    string_with_space: true,
+    length: { maximum: 160 },
+    allow_nil: false
+
+  validates :research_interests_summary,
+    presence: true,
+    length: { maximum: 4000 },
+    allow_nil: false
+
+  validate :start_proceeds_termination
+
+  def start_proceeds_termination
+    unless self.role_termination_date.nil? or self.role_termination_date > self.role_start_date
+      errors.add(:termination_date, "termination date cannot be prior to join date")
+    end
+  end
 end
