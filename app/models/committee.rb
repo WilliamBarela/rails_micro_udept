ebd0227e90657fb245058e007871fd52aea22421
William Barela <william.barela.dev@gmail.com>
2019-05-09 12:39:47 -0500

    add validation to limit string and text

--- app/models/committee.rb
+++ app/models/committee.rb
@@ -2,12 +2,21 @@ class Committee < ApplicationRecord
   has_many :committee_memberships
   has_many :people, through: :committee_memberships
 
-  validates :committee_title, :committee_description,
-    presence: true, string_with_space: true
+  validates :committee_title
+    presence: true,
+    string_with_space: true,
+    length: { maximum: 255}
+
+  validates :committee_description,
+    presence: true,
+    string_with_space: true,
+    length: { maximum: 500 }
 
   validates :committee_started,
-    presence: true, date_not_in_future: true
+    presence: true,
+    date_not_in_future: true
 
   validates :committee_depricated,
-    date_not_in_future: true, allow_nil: true
+    date_not_in_future: true,
+    allow_nil: true
 end
