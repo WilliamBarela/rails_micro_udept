8808b1715e8b040c024e77af9515628f6641b142
William Barela <william.barela.dev@gmail.com>
2019-05-09 09:49:34 -0500

    ammend to replace if with unless to allow nil

--- app/validators/date_not_in_past_validator.rb
+++ app/validators/date_not_in_past_validator.rb
@@ -1,6 +1,6 @@
 class DateNotInPastValidator < ActiveModel::EachValidator
   def validate_each(record, attribute, value)
-    if value < Date.today
+    unless value.nil? or value >= Date.today
       record.errors[attribute] << (options[:message] || "date cannot be in the past.")
     end
   end
