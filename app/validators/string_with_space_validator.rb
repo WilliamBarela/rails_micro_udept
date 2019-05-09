c9055e879799b9a3020123736587b9b299f48739
William Barela <william.barela.dev@gmail.com>
2019-05-09 12:31:57 -0500

    amend validator with unless to allow for nil value

--- app/validators/string_with_space_validator.rb
+++ app/validators/string_with_space_validator.rb
@@ -1,6 +1,6 @@
 class StringWithSpaceValidator < ActiveModel::EachValidator
   def validate_each(record, attribute, value)
-    unless value =~ /\A[a-zA-Z0-9\- ]+\z/
+    unless value.nil? or value =~ /\A[a-zA-Z0-9\- ]+\z/
       record.errors[attribute] << (options[:message] || "only alphanumeric characters, dashes and spaces allowed")
     end
   end
