434b20605a87cb0b1311a013d8f02d906b282c7f
William Barela <william.barela.dev@gmail.com>
2019-05-09 12:31:07 -0500

    add validations to staff_members

--- app/models/staff_member.rb
+++ app/models/staff_member.rb
@@ -1,13 +1,23 @@
 class StaffMember < ApplicationRecord
   belongs_to :person
 
-  # validations to complete
-  #:current_title
-  #:auxiliary_roles
-  #:departmental?
-  #:objectives_statement
-  #:departmental_impact_statement
-  #:role_termination_date
-  #:termination_reason
-  #:person
+  validates :current_title, :auxiliary_roles, :termination_reason,
+    presence: true,
+    string_with_space: true,
+    length: { maximum: 255 },
+    allow_nil: false
+
+  validates :objectives_statement, 
+    presence: true,
+    string_with_space: true,
+    length: { maximum: 160 },
+    allow_nil: false
+
+  validates :departmental_impact_statement, 
+    presence: true,
+    length: { maximum: 4000 },
+    allow_nil: false
+
+  validates :departmental?,
+    inclusion: { in: [ true, false] }
 end
