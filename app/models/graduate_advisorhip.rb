dd498ffcc5ff1bc536f97af06467ae2bdfb62345
William Barela <william.barela.dev@gmail.com>
2019-05-10 08:50:16 -0500

    add validations to graduate_advisorship

--- app/models/graduate_advisorhip.rb
+++ app/models/graduate_advisorhip.rb
@@ -2,10 +2,15 @@ class GraduateAdvisorhip < ApplicationRecord
   belongs_to :faculty_member
   belongs_to :graduate_student
 
-  # items to validate
-  #:primary_advisor?
-  #:advising_initiation
-  #:advising_termination
-  #:faculty_member
-  #:graduate_student
+  validates :primary_advisor?,
+    presence: true,
+    inclusion: { in: [ true, false ] }
+
+  validate :start_proceeds_termination
+
+  def start_proceeds_termination
+    unless self.advising_termination.nil? or self.advising_termination > self.advising_initiation
+      errors.add(:advising_termination, "termination date cannot be prior to join date")
+    end
+  end
 end
