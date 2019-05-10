5ec18c8ef810482c54b177c5b311b11a021b1534
William Barela <william.barela.dev@gmail.com>
2019-05-09 19:58:16 -0500

    add validations to graduate_students

--- app/models/graduate_student.rb
+++ app/models/graduate_student.rb
@@ -4,15 +4,36 @@ class GraduateStudent < ApplicationRecord
   has_many :graduate_advisorships
   has_many :faculty_members, through: :graduate_advisorships
 
-  # validations to make
-  #:current_program
-  #:degree_sought
-  #:auxiliary_roles
-  #:expected_graduation_year
-  #:expected_graduation_semester
-  #:objectives_statement
-  #:research_interests_summary
-  #:role_termination_date
-  #:termination_reason
-  #:person
+  validates :current_program, :degree_sought, :auxiliary_roles, :termination_reason,
+    presence: true,
+    string_with_space: true,
+    length: { maximum: 255 },
+    allow_nil: false
+
+  validates :expected_graduation_year,
+    presence: true,
+    inclusion: { in: Date.today.year..(Date.today.year + 10) }
+
+  validates :expected_graduation_semester,
+    presence: true,
+    inclusion: { in: [fall, spring, summer] }
+
+  validates :objectives_statement
+    presence: true,
+    string_with_space: true,
+    length: { maximum: 160 },
+    allow_nil: false
+
+  validates :research_interests_summary,
+    presence: true,
+    length: { maximum: 4000 },
+    allow_nil: false
+
+  validate :start_proceeds_termination
+
+  def start_proceeds_termination
+    unless self.role_termination_date.nil? or self.role_termination_date > self.role_start_date
+      errors.add(:role_termination_date, "termination date cannot be prior to join date")
+    end
+  end
 end
